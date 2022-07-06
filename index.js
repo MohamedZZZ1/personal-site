b4b5e2eb8200a9f5b605ad2ec784ffa3956b5b46
MohamedZZZ1 <108814962+MohamedZZZ1@users.noreply.github.com>
2022-07-06 15:33:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("fantasy")
+favouriteMovieGenre("space")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
 favouriteFruit("orange")
