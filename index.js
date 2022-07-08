1d7b53cd0c5fce9dc1bfa846df3ec14e09358aa8
MohamedZZZ1 <108814962+MohamedZZZ1@users.noreply.github.com>
2022-07-08 12:43:03 +0100

    W

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("superhero")
+favouriteMovieGenre("cowboy")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
-favouriteFruit("blueberry")
+favouriteFruit("banana")
 
 // light, dark
-favouriteMode("dark")
+favouriteMode("light")
 
 // sharp, soft, round
 favouriteEdgeStyle("soft")
